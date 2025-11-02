8e00dfbbcc7a0bc133a949035e146d894e61575c
he0xa1 <68824365+he0xA1@users.noreply.github.com>
2025-11-03 03:00:23 +0330

    refactor: streamline file processing in git functions and improve code clarity

--- src/git.ts
+++ src/git.ts
@@ -1,29 +1,38 @@
 import { execSync } from "node:child_process";
 
 function getListsOfStagedFiles(): string[] {
-  const gitDiffFiles = execSync("git --no-pager diff --cached --name-only").toString();
+  const gitDiffFiles = execSync(
+    "git --no-pager diff --cached --name-only",
+  ).toString();
   return gitDiffFiles
     .split("\n")
     .map((file) => file.trim())
     .filter((file) => file.length > 0);
 }
 
-function extractFullHistoryOfEachFile(files: string[]): Record<string, string> {
+function extractFullHistoryOfEachFile(): Record<string, string> {
   const fileContents: Record<string, string> = {};
-  for (const file of files) {
+  for (const file of getListsOfStagedFiles()) {
     const content = execSync(`git --no-pager log -p -- ${file}`).toString(); // it's can be most recent changes only
     fileContents[file] = content;
   }
   return fileContents;
 }
 
-function getDiffOfStagedFiles() {
-  const gitDiffOutput = execSync("git --no-pager diff --cached").toString();
-  const diffSections = gitDiffOutput.split("diff --git");
-  console.log(diffSections);
+function getDiffOfStagedFiles(): Record<string, string> {
+  const stagedChanges: Record<string, string> = {};
 
+  for (const file of getListsOfStagedFiles()) {
+    const gitDiffOutput = execSync(
+      `git --no-pager diff --cached ${file} `,
+    ).toString();
+    stagedChanges[file] = gitDiffOutput;
+  }
+  return stagedChanges;
 }
 
-getDiffOfStagedFiles();
-
-export { getListsOfStagedFiles, extractFullHistoryOfEachFile };
+export {
+  getListsOfStagedFiles,
+  extractFullHistoryOfEachFile,
+  getDiffOfStagedFiles,
+};
