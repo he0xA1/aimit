a87871122545800e4e1abccb78de08c86fb3227c
he0xa1 <68824365+he0xA1@users.noreply.github.com>
2025-11-05 01:27:27 +0330

    refactor: Rename logDebug, logInfo, and logWarn functions

--- src/logger.ts
+++ src/logger.ts
@@ -6,19 +6,19 @@ export function setUpLogger(setting: LogSetting) {
   logSetting = { ...logSetting, ...setting };
 }
 
-export function debug(...args: unknown[]) {
+export function logDebug(...args: unknown[]) {
   if (!logSetting.quiet && logSetting.verbose) {
     console.debug("Debug:", ...args);
   }
 }
 
-export function info(...args: unknown[]) {
+export function logInfo(...args: unknown[]) {
   if (!logSetting.quiet) {
     console.info("Info:", ...args);
   }
 }
 
-export function warn(...args: unknown[]) {
+export function logWarn(...args: unknown[]) {
   if (!logSetting.quiet) {
     console.warn("Warning:", ...args);
   }
